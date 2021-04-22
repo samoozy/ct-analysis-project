53f2911e0bf61564d73271d748cf8f9d03343c01
Hiro <s4moozy@gmail.com>
2021-04-22 16:43:20 +0900

    Added base styling for modal popup

---
+++ src/components/layout/TheModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="isOpen">
+    <div 
+      class="fixed top-0 left-0 w-full h-full bg-black opacity-50"
+      @click="$emit('onClick')"
+    ></div>
+
+    <div class="bg-white top-1/2 left-1/2 w-72 h-96 fixed -mt-48 -ml-36 sm:w-96 sm:-ml-48 rounded p-4">
+      <div class="flex justify-end">
+        <button 
+          class="focus:outline-none"
+          @click="$emit('onClick')"
+        >
+          <svg 
+            class="fill-current text-indigo-600"
+            xmlns="http://www.w3.org/2000/svg" 
+            width="18" 
+            height="18" 
+            viewBox="0 0 24 24"
+          ><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+        </button>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: Boolean
+  },
+  emits: ['onClick']
+}
+</script>
--- src/components/ui/NavbarButton.vue
+++ src/components/ui/NavbarButton.vue
@@ -1,27 +1,35 @@
 <template>
   <button 
-    v-if="mode === 'primary'"
-    class="bg-indigo-600 px-3 py-2 rounded hover:bg-indigo-800 duration-200 text-white text-sm focus:outline-none"
+    :class="{
+      'bg-indigo-600 px-3 py-2 rounded hover:bg-indigo-800 duration-200 text-white text-sm focus:outline-none' : filled,
+      'px-2 py-2 text-indigo-600 rounded text-sm hover:text-indigo-400 duration-200 border border-indigo-600 hover:border-indigo-400 focus:outline-none' : ghost
+    }"
+    @click="$emit('emitClick')"
   >
     <div class="flex items-center">
       <slot></slot>
     </div>
-  </button>
-
-  <button 
-    v-if="mode === 'secondary'"
-    class="px-2 py-2 text-indigo-600 rounded text-sm hover:text-indigo-400 duration-200 border border-indigo-600 hover:border-indigo-400 focus:outline-none"
-  >
-    <div class="flex items-center">
-      <slot></slot>
-    </div>
+    
   </button>
 </template>
 
 <script>
 export default {
   props: {
-    mode: String
-  }
+    mode: {
+      type: String,
+      required: true,
+      default: 'filled'
+    },
+  },
+  computed: {
+    filled() {
+      return this.mode === 'filled'
+    },
+    ghost() {
+      return this.mode === 'ghost'
+    }
+  },
+  emits: ['emitClick']
 }
 </script>
