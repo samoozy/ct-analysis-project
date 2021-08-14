de852332fdfb987fc2bea1d70ba32b2e244780cc
Keipen123 <CBeTpenguin@gmail.com>
2021-08-14 16:32:02 +0900

    changed logo layouts

--- src/components/lp/LogoSection.vue
+++ src/components/lp/LogoSection.vue
@@ -1,45 +1,45 @@
 <template>
   <section>
-    <h3 class="text-3xl font-extrabold font-nunito-sans text-gray-700 text-center mb-20">
+    <h3 class="text-3xl font-extrabold font-nunito-sans text-gray-700 text-center mb-10">
       CT Analysis <span class="text-2xl">掲載プロジェクト</span>
     </h3>
     <div class="flex flex-wrap flex-auto items-center justify-evenly">
-      <div class="content Binance flex-1-45 md:flex-1-22">
+      <div class="content Binance flex-1-42 lg:flex-1-15">
         <img src="@/assets/project_logos/d44818-6-851058-0.png" alt="Binance" />
       </div>
-      <div class="content Etherum flex-1-30 md:flex-1-15 ml-10 mr-5">
+      <div class="content Etherum flex-1-30 ml-10 mr-5 lg:flex-1-10 lg:mr-10">
         <img src="@/assets/project_logos/ethereum.svg" alt="Etherum" />
       </div>
-      <div class="content Huobi flex-1-45 md:flex-1-22 m-1 pr-1">
+      <div class="content Huobi flex-1-42 m-1 mt-12 pr-1 lg:flex-1-20 lg:mr-16 lg:mt-0">
         <img src="@/assets/project_logos/440px-Huobi-logo.png" alt="Huobi" />
       </div>
-      <div class="content Bithumb flex-1-45 md:flex-1-22 mt-2 m-1">
+      <div class="content Bithumb flex-1-45 mt-16 m-1 lg:flex-1-22 lg:m-2 lg:mt-4">
         <img src="@/assets/project_logos/Bithumb_logo.png" alt="Bithumb" />
       </div>
-      <div class="content Coinbase flex-1-45 md:flex-1-22">
+      <div class="content Coinbase flex-1-45 mt-12 mr-5 lg:flex-1-20">
         <img src="@/assets/project_logos/Coinbase.png" alt="Coinbase" />
       </div>
-      <div class="content Cosmos flex-1-45 md:flex-1-22 mt-2">
+      <div class="content Cosmos flex-1-45 mt-16 mr-3 lg:flex-1-24 lg:mx-32 lg:mr-0">
         <img src="@/assets/project_logos/cosmos-logo-black.png" alt="Cosmos" />
       </div>
-      <div class="content Kusama flex-1-45 md:flex-1-22">
+      <div class="content Kusama flex-1-45 mt-12 lg:flex-1-22 lg:m-3 lg:ml-14 lg:mt-16">
         <img src="@/assets/project_logos/KUSAMA_logo_1.png" alt="Kusama" />
       </div>
-      <div class="content Solana flex-1-55 md:flex-1-30 mt-1.5 pl-2">
+      <div class="content Solana flex-1-55 mt-16 pl-4 lg:flex-1-30 lg:mt-12">
         <img src="@/assets/project_logos/OMnvVEz.png" alt="Solana" />
       </div>
-      <div class="content SymbolNem flex-1-40 md:flex-1-22">
+      <div class="content SymbolNem flex-1-40 mt-16 lg:flex-1-22 lg:mr-20 lg:ml-24 lg:mt-16">
         <img
           src="@/assets/project_logos/Symbol_from_nem.png"
           alt="Symbol from Nem"
         />
       </div>
-      <div class="content Polkadot flex-1-45 md:flex-1-22 ml-4 mb-3">
-        <img src="@/assets/project_logos/ダウンロード.png" alt="Polkadot" />
+      <div class="content Polkadot flex-1-45 mt-12 ml-4 lg:flex-1-22 lg:mr-8 lg:ml-0">
+        <img src="@/assets/project_logos/Polkadot.png" alt="Polkadot" />
       </div>
     </div>
 
-    <div class="flex justify-end">
+    <div class="flex justify-end mt-5">
       <p class="text-gray-600">上記は一例です</p>
     </div>
   </section>
